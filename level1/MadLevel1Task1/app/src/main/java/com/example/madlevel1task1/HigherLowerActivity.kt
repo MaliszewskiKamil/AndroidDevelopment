22dde832c572088a499d455f802bd73877be1c1b
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 17:05:19 +0200

    Added some Ui logic implemented roll dice and init views methods

--- level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -1,15 +1,46 @@
 package com.example.madlevel1task1
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 private lateinit var binding: ActivityHigherLowerBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.equalBtn.setOnClickListener{
+            updateUi()
+        }
     }
+
+    private fun initViews(){
+        updateUi()
+    }
+
+    private fun updateUi() {
+        binding.lastThrowTv.text = getString(R.string.last_throw, lastThrow)
+        var imageName = "R.drawable.dice"
+        imageName += lastThrow
+        binding.diceRollIv.setImageResource(resources.getIdentifier(imageName, "drawable", packageName))
+    }
+
+    private fun rollDice(){
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUi()
+    }
+
+
 }
