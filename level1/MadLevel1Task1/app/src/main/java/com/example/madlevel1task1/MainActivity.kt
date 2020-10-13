1ff3e516467b07eba6c341ebd51f15d3d91eccbc
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 13:58:25 +0200

    init of task1 with added binding, JetPack and debuggable

---
+++ level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.madlevel1task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madlevel1task1.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
