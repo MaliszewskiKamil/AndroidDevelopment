65438ddfd962d07ca6724bc2f5cc82439fdc1009
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 22:25:18 +0200

    Implemented MainActivity class

--- level2/MadLevel2Example/app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ level2/MadLevel2Example/app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -2,15 +2,47 @@ package com.example.madlevel2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnAddReminder.setOnClickListener{
+            val reminder = binding.etReminder.text.toString()
+            addReminder(reminder)
+        }
+
+        binding.rvReminders.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
+
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+    }
+
+    private fun addReminder(reminder: String) {
+        if(reminder.isNotBlank()){
+            reminders.add(Reminder(reminder))
+            reminderAdapter.notifyDataSetChanged()
+            binding.etReminder.text?.clear()
+        } else {
+            Snackbar.make(binding.etReminder, "Empty reminder", Snackbar.LENGTH_LONG).show()
+        }
+
     }
 }
