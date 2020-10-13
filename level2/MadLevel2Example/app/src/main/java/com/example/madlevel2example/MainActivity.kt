fc9cfd74b3714e990c3d057f1bb976a1427e5959
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 21:19:06 +0200

    init of level2 example + applied binding

---
+++ level2/MadLevel2Example/app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.madlevel2example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madlevel2example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
