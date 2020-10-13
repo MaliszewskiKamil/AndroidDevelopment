e964d491747ec95042533db3fd370ed18d635421
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 16:21:22 +0200

    renamed activities + added images

--- level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,15 +2,14 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.madlevel1task1.databinding.ActivityMainBinding
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
-private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityHigherLowerBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
