30b91e5d28ea0285204ecc6387587e3c3a23fc3e
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 18:57:20 +0200

    Added logic to the software

--- level1/MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
+++ level1/MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
@@ -2,14 +2,52 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task2.databinding.ActivityTruthTableBinding
 
 private lateinit var binding: ActivityTruthTableBinding
 
 class TruthTableActivity : AppCompatActivity() {
+
+    private var points: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTruthTableBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
     }
+
+    private fun initView(){
+        binding.submitBtn.setOnClickListener{
+            onButtonClick()
+        }
+    }
+
+    private fun resetPoints(){
+        points = 0
+    }
+
+    private fun showToastWithPoints(){
+        Toast.makeText(this, getString(R.string.correct_answers, points), Toast.LENGTH_LONG).show()
+    }
+
+    private fun checkAnswers(){
+        if(binding.firstEv.text.toString() == "T")
+            points++
+        if(binding.secondEv.text.toString() == "F")
+            points++
+        if(binding.thirdEv.text.toString() == "F")
+            points++
+        if(binding.fourthEv.text.toString() == "F")
+            points++
+
+    }
+
+    private fun onButtonClick(){
+        checkAnswers()
+        showToastWithPoints()
+        resetPoints()
+    }
+
 }
