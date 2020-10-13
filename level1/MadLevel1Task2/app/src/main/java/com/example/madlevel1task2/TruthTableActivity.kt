4815e80128542d538cf7817b6abec2722a3bc826
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-13 17:34:18 +0200

    added binding to the app

--- level1/MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
+++ level1/MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
@@ -2,10 +2,14 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madlevel1task2.databinding.ActivityTruthTableBinding
+
+private lateinit var binding: ActivityTruthTableBinding
 
 class TruthTableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_truth_table)
+        binding = ActivityTruthTableBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
