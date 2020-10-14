d0747229ead075f9840be50d8688ec087115c8b0
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-14 11:55:44 +0200

    implemented required methods for ViewHolder

--- level2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/adapters/PlaceAdapter.kt
+++ level2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/adapters/PlaceAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.madlevel2task1.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.R
 import com.example.madlevel2task1.data.Place
 import com.example.madlevel2task1.databinding.ItemPlaceBinding
 
@@ -20,5 +23,17 @@ class PlaceAdapter(private val places: List<Place>) :
         }
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(places[position])
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
 }
