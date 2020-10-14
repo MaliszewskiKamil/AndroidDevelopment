03b058c4b702739d21726ac0089f363e3438b7a7
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-14 12:10:42 +0200

    Implemented initViews, but nothing shows on screen

--- level2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/PlacesActivity.kt
+++ level2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/PlacesActivity.kt
@@ -2,14 +2,31 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task1.adapters.PlaceAdapter
+import com.example.madlevel2task1.data.Place
 import com.example.madlevel2task1.databinding.ActivityPlacesBinding
 
 private lateinit var binding: ActivityPlacesBinding
+private val places = arrayListOf<Place>()
+private val placesAdapter = PlaceAdapter(places)
 
 class PlacesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.placesRv.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+
+        for(i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placesAdapter.notifyDataSetChanged()
+
     }
 }
