c6a0bbc8e8e8d2ae90c949f8eea053ef44611999
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-14 13:21:21 +0200

    Added data class

---
+++ level2/MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -0,0 +1,23 @@
+package com.example.madlevel2task2
+
+data class Question(var questionText: String) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "A 'val' and 'var' are the same.",
+            "Mobile Application Development grants 12 ECTS.",
+            "A Unit in Kotlin corresponds to void in Java.",
+            "In Kotlin 'when' replaces 'switch' operator in Java",
+            "Adapters are awesome",
+            "I will pass this course"
+        )
+
+        val ANSWERS = arrayOf(
+            true,
+            false,
+            false,
+            true,
+            true,
+            true
+        )
+    }
+}
