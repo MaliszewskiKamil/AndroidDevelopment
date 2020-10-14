402b967121eb41558dc68e6d8140abcea1d2c223
MaliszewskiKamil <kamcio4112@gmail.com>
2020-10-14 13:31:24 +0200

    Finished adapter

--- level2/MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/adapters/QuestionAdapter.kt
+++ level2/MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/adapters/QuestionAdapter.kt
@@ -1,27 +1,37 @@
 package com.example.madlevel2task2.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task2.R
 import com.example.madlevel2task2.data.Question
+import com.example.madlevel2task2.databinding.QuestionItemBinding
 
 class QuestionAdapter(private val questions: List<Question>) :
     RecyclerView.Adapter<QuestionAdapter.ViewHolder>(){
 
     inner class ViewHolder(itemView: View):
             RecyclerView.ViewHolder(itemView){
+        private val binding =  QuestionItemBinding.bind(itemView)
+
+        fun databind(question: Question){
+            binding.questionTv.text = question.questionText
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.question_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.databind(questions[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return questions.size
     }
 
 }
