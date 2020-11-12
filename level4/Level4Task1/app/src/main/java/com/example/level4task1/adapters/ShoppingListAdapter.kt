76f691c0cd5f0ca7c537da9adfc704c407a668d6
MaliszewskiKamil <kamcio4112@gmail.com>
2020-11-12 23:26:38 +0100

    added menu design + product data class

---
+++ level4/Level4Task1/app/src/main/java/com/example/level4task1/adapters/ShoppingListAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.level4task1.adapters
+
+import com.example.level4task1.data.Product
+
+class ShoppingListAdapter(private val products: List<Product>)
---
+++ level4/Level4Task1/app/src/main/java/com/example/level4task1/data/Product.kt
@@ -0,0 +1,17 @@
+package com.example.level4task1.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "productTable")
+data class Product (
+    @ColumnInfo
+    var productText: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id")
+    var id: Long? = null
+
+
+)
