29138932af01f5dfed2699414fb8d4bd6bbf13ab
MaliszewskiKamil <kamcio4112@gmail.com>
2020-11-13 00:17:44 +0100

    implemented some methods in shoppinglist fragment

--- level4/Level4Task1/app/src/main/java/com/example/level4task1/ShoppingListFragment.kt
+++ level4/Level4Task1/app/src/main/java/com/example/level4task1/ShoppingListFragment.kt
@@ -7,13 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.level4task1.adapters.ShoppingListAdapter
+import com.example.level4task1.data.Product
+import com.example.level4task1.database.ProductRepository
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.shopping_list_fragment.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ShoppingListFragment : Fragment() {
+
+    private val products = arrayListOf<Product>()
+    private val shoppingListAdapter = ShoppingListAdapter(products)
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +40,62 @@ class ShoppingListFragment : Fragment() {
         return inflater.inflate(R.layout.shopping_list_fragment, container, false)
     }
 
+    private lateinit var productRepository: ProductRepository
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        productRepository = ProductRepository(requireContext())
 
 
+        initRv()
+
+    }
+
+
+    private fun initRv() {
+        rv_shopping_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rv_shopping_list.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+        rv_shopping_list.adapter = shoppingListAdapter
+        rv_shopping_list.apply {
+            setHasFixedSize(true)
+        }
+
+
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val productToDelete = products[position]
+                CoroutineScope(Dispatchers.Main).launch {
+                    val reminders = withContext(Dispatchers.IO) {
+                        productRepository.deleteProduct(productToDelete)
+                    }
+                    getProductsFromDatabase()
+                }
+            }
+
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    private fun getProductsFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@ShoppingListFragment.products.clear()
+            this@ShoppingListFragment.products.addAll(products)
+            this@ShoppingListFragment.shoppingListAdapter.notifyDataSetChanged()
+        }
     }
 }
