8c24debc766f9bd396f2c342147d9263649fea14
MaliszewskiKamil <kamcio4112@gmail.com>
2020-11-13 00:01:47 +0100

    changed names

--- level4/Level4Task1/app/src/main/java/com/example/level4task1/ShoppingListFragment.kt
+++ level4/Level4Task1/app/src/main/java/com/example/level4task1/ShoppingListFragment.kt
@@ -7,25 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ShoppingListFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        return inflater.inflate(R.layout.shopping_list_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 }
--- level4/Level4Task1/app/src/main/java/com/example/level4task1/database/ProductRepository.kt
+++ level4/Level4Task1/app/src/main/java/com/example/level4task1/database/ProductRepository.kt
@@ -1,2 +1,29 @@
 package com.example.level4task1.database
 
+import android.content.Context
+import com.example.level4task1.data.Product
+
+class ProductRepository(context: Context){
+    private val productDao: ProductDao
+
+    init {
+        val database = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+    }
+
+    suspend fun getAllProducts(): List<Product> {
+        return productDao.getAllProducts()
+    }
+
+    suspend fun insertProduct(product: Product){
+        productDao.insertProduct(product)
+    }
+
+    suspend fun deleteProduct(product: Product){
+        productDao.deleteProduct(product)
+    }
+
+    suspend fun deleteAllProducts(){
+        productDao.deleteAllProducts()
+    }
+}
